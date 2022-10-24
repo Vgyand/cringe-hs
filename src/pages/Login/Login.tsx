5bd7672640b23c7148f791381d58338c7bee8a01
Vgyand <gyandzhunczevv@gmail.com>
2022-10-24 13:42:05 +0300

    basic forms validation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,7 @@
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { toastr } from 'react-redux-toastr'
+
+import { FormValues } from '@/shared/types/formTypes'
 
 import styles from './Login.module.scss'
 
@@ -8,28 +10,55 @@ const Login = () => {
 		register,
 		formState: { errors },
 		handleSubmit,
-	} = useForm()
+	} = useForm<FormValues>({
+		mode: 'onBlur',
+	})
 
-	const onSubmit = (data: any) => {
+	console.log(errors)
+
+	const onSubmit: SubmitHandler<FormValues> = (data) => {
 		toastr.success('User has been autherized', `${data.login}`)
+		//login function will be implemented
 	}
 
 	return (
 		<div className={styles.login}>
 			<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+				<h1 className="text-white text-3xl mx-auto">Login</h1>
 				<label>
 					Login
 					<input
 						{...register('login', {
-							required: true,
+							required: 'Is required',
+							minLength: {
+								value: 6,
+								message: 'Min 6 symbols',
+							},
 						})}
 						className={styles.formInput}
 					/>
+					{errors?.login && (
+						<p className="text-primary">{errors.login.message}</p>
+					)}
 				</label>
 				<label>
 					Password
-					<input type="password" className={styles.formInput} />
+					<input
+						{...register('password', {
+							required: 'Is required',
+							minLength: {
+								value: 6,
+								message: 'Password is not hard enough',
+							},
+						})}
+						type="password"
+						className={styles.formInput}
+					/>
+					{errors?.password && (
+						<p className="text-primary">{errors.password.message}</p>
+					)}
 				</label>
+
 				<input type="submit" className={styles.formBtn} />
 			</form>
 		</div>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,7 +1,68 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { toastr } from 'react-redux-toastr'
+
+import { FormValues } from '@/shared/types/formTypes'
+
 import styles from './Register.module.scss'
 
 const Register = () => {
-	return <div className={styles.register}>Register</div>
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<FormValues>({
+		mode: 'onBlur',
+	})
+
+	console.log(errors)
+
+	const onSubmit: SubmitHandler<FormValues> = (data) => {
+		toastr.success('User has been registered', `${data.login}`)
+		//register function will be implemented
+	}
+
+	return (
+		<div className={styles.register}>
+			<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+				<h1 className="text-white text-3xl mx-auto">Register</h1>
+				<label>
+					Login
+					<input
+						{...register('login', {
+							required: 'Is required',
+							minLength: {
+								value: 6,
+								message: 'Min 6 symbols',
+							},
+						})}
+						className={styles.formInput}
+					/>
+					{errors?.login && (
+						<p className="text-primary">{errors.login.message}</p>
+					)}
+				</label>
+				<label>
+					Password
+					<input
+						{...register('password', {
+							required: 'Is required',
+							minLength: {
+								value: 6,
+								message: 'Password is not hard enough',
+							},
+						})}
+						type="password"
+						className={styles.formInput}
+					/>
+					{errors?.password && (
+						<p className="text-primary">{errors.password.message}</p>
+					)}
+				</label>
+
+				<input type="submit" className={styles.formBtn} />
+			</form>
+		</div>
+	)
 }
 
 export default Register
